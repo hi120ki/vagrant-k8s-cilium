86168e49a4855ffb71f9d963ef70aa916d30dc84
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2023-02-02 14:49:56 +0900

    fix
    Signed-off-by: Hi120ki <12624257+hi120ki@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ echo "[+] coredns pending done"
 echo "[i] install cilium helm"
 helm repo add cilium https://helm.cilium.io/
 helm repo update
-helm install cilium cilium/cilium --version 1.12.5 --namespace kube-system --set operator.replicas=1
+helm install cilium cilium/cilium --version 1.12.6 --namespace kube-system --set operator.replicas=1
 
 echo "[i] install cilium cli"
 CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
@@ -119,7 +119,6 @@ echo "[i] node info"
 kubectl get nodes -o wide
 
 echo "[i] enable hostpath provisioner"
-sudo snap install yq
 sudo cat /etc/kubernetes/manifests/kube-controller-manager.yaml |
   yq -e '.spec.containers[].command += ["--enable-hostpath-provisioner=true"]' |
   sudo tee /etc/kubernetes/manifests/kube-controller-manager.yaml
